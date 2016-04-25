835fe137e4f4600056d468bfcb6d982043ac42cd
Jeff Nappi <jeff@clearvoice.com>
2016-04-24 20:08:10 -0700

    Add spaghetti example, start refactored basics

---
+++ 02_preparing_for_angular_2/02-refactored_basics/app.js
@@ -0,0 +1,38 @@
+/**
+ * SPAGHETTI CODE EXAMPLE!!! DO NOT USE
+ */
+
+angular.module('app', [])
+  .controller('Home',
+    ['$scope', function ($scope) {
+      $scope.title = 'Ultimate Team Manager';
+    }]);
+
+angular.module('app')
+  .controller('Teams',
+    ['$scope', '$http', function ($scope, $http) {
+      $scope.teams = [];
+      $scope.newTeam = '';
+
+      $scope.deleteTeam = function (index) {
+        $scope.teams.splice(index, 1);
+      };
+
+      $scope.addTeam = function () {
+        if (!$scope.newTeam) {
+          alert('No name provided');
+          return;
+        }
+        $scope.teams.push($scope.newTeam);
+        $scope.newTeam = '';
+        $scope.title = 'Ultimate Team Manager: ' +
+          $scope.teams.length + ' Teams';
+      };
+
+      $http.get('api/teams.json').then(
+        function (response) {
+          var teams = response.data;
+          $scope.teams = teams.data
+        }
+      )
+    }]);
